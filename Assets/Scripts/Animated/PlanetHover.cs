017817733c4e8773fb3c1efc3410e83c080dd7f0
Evgeniy Schemelinin <Kovarniy@users.noreply.github.com>
2019-09-27 10:35:55 +0800

    add Space

---
+++ Assets/Scripts/Animated/PlanetHover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanetHover : MonoBehaviour
+{
+
+    private Color basicColor = Color.white;
+    private Color hoverColor = Color.red;
+    private Renderer renderer;
+    private Image thisImage;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = basicColor;
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Image>().color = hoverColor;
+        // renderer.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Image>().color = basicColor;
+        // renderer.material.color = basicColor;
+    }
+}
